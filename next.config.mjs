8123cc414f3116bb0dcdc9093e983da099ede24d
Brandon3890 <bra.caroca@duocuc.cl>
2026-06-24 18:13:24 -0400

    Cambios productos 16

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,31 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+
   images: {
     unoptimized: true,
+  },
+
+  async headers() {
+    return [
+      {
+        source: '/uploads/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-cache, no-store, must-revalidate',
+          },
+          {
+            key: 'Pragma',
+            value: 'no-cache',
+          },
+          {
+            key: 'Expires',
+            value: '0',
+          },
+        ],
+      },
+    ]
   },
 }
 
